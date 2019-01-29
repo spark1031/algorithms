938801c0df5e981fc3128d074053a68524e48d93
Sujin Park <sujinpark1031@gmail.com>
2019-01-28 17:38:05 -0800

    Edit bst iterator soln

--- leetcode/bst-iterator.js
+++ leetcode/bst-iterator.js
@@ -45,3 +45,56 @@ BSTIterator.prototype.hasNext = function () {
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+
+//RECURSIVE SOLUTION!!
+/**
+* Definition for binary tree
+* function TreeNode(val) {
+*     this.val = val;
+*     this.left = this.right = null;
+* }
+*/
+
+// /**
+//  * @constructor
+//  * @param {TreeNode} root - root of the binary search tree
+//  */
+var BSTIterator = function (root) {
+  this.array = []
+  dfs(root, this.array)
+
+  function dfs(root, array) {
+    if (!root) {
+      return
+    }
+    dfs(root.left, array)
+    array.push(root.val)
+    dfs(root.right, array)
+  }
+};
+
+
+// /**
+//  * @this BSTIterator
+//  * @returns {boolean} - whether we have a next smallest number
+//  */
+BSTIterator.prototype.hasNext = function () {
+  return this.array.length > 0
+};
+
+// /**
+//  * @this BSTIterator
+//  * @returns {number} - the next smallest number
+//  */
+BSTIterator.prototype.next = function () {
+  if (this.hasNext()) {
+    return this.array.shift()
+  }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * var i = new BSTIterator(root), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
