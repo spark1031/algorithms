db137c8f3844d1d283b42755a6a0f1392e29bd02
Sujin Park <sujinpark1031@gmail.com>
2019-02-04 17:46:39 -0800

    complete Array 1.1, O(n^2) time complexity

--- ctci/arrays.js
+++ ctci/arrays.js
@@ -1,8 +1,13 @@
-//1.1)
+//1.1
 function isUnique(string) {
   for (let i = 0; i < string.length; i++) {
+    let currentLetter = string[i];
     for (let j = i + 1; j < string.length; j++) {
-
+      let comparisionLetter = string[j];
+      if (currentLetter === comparisionLetter) {
+        return false;
+      }
     }
   }
+  return true;
 }
